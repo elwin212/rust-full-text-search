e04d4b709c32df078027910380c262ca1a7c53db
Elwin <elwin@mmisys.net>
2023-09-10 20:15:03 -0700

    fix bug

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::io;
 extern crate quick_xml;
 use std::time::Instant;
@@ -10,34 +10,19 @@ fn main() {
     let documents = document::load_file("data/data.xml");
     match documents {
         Ok(documents) => {
-            let mut index_docs: HashMap<i32, document::Item> = HashMap::new();
-            for (id, item) in documents.doc.iter().enumerate() {
-                let id = id as i32 + 1; // Assign IDs starting from 1
-                index_docs.insert(id, item.clone());
-            }
+            let index_docs = document::assign_index_to_docs(&documents);
             let inverted_index = document::build_inverted_index(&index_docs);
-            /*let now = Instant::now();
-            let result1 = search_using_contains(&documents.doc, "test");
-            let elapsed = now.elapsed();
-            println!("Elapsed: {:.6?}", elapsed);
-            println!("Search: test; Len: {}; First: {}", result1.len(), documents.doc[result1[0]].title);
+            performance_comparison(documents, inverted_index, index_docs);            
+        },
+        Err(err) => {
+            println!("Error: {:?}", err);
+        }
+    } 
+}
 
-            let now = Instant::now();
-            let result2 = search_using_regex(&documents.doc, "test");
-            let elapsed = now.elapsed();
-            println!("Search: test; Len: {}; First: {}", result2.len(), documents.doc[result2[0]].title);
-            println!("Elapsed: {:.6?}", elapsed);
-            
-            
-            
-            let now = Instant::now();
-            let result3 = inverted_index.get("test");
-            let elapsed = now.elapsed();
-            println!("Elapsed: {:.6?}", elapsed);
-            println!("Search: test; Len: {}", result3.unwrap().len());
-            */
-            loop {
-                println!("Enter a command (or 'quit' to exit):");
+fn performance_comparison(documents: document::Documents, inverted_index: HashMap<String, HashSet<i32>>, index_docs: HashMap<i32, document::Item>) {
+    loop {
+        println!("Enter a command (or 'quit' to exit):");
                 let mut input = String::new();
                 // Read user input
                 io::stdin()
@@ -48,15 +33,35 @@ fn main() {
                 if input == "quit" {
                     // Exit the loop when the user enters 'quit'
                     break;
-                } else {            
+                } else {          
+                    /* Search using string contain */
+                    let now = Instant::now();
+                    let result1 = document::search_using_contains(&documents.doc, &input);
+                    let elapsed = now.elapsed();
+                    for ids in result1 {
+                        let i32_ids = ids as i32;
+                        println!("Result using string contains: {:?}", documents.doc.get(i32_ids as usize).unwrap().title);
+                    }                        
+                    println!("Elapsed: {:.6?}", elapsed);
+
+                    /* Search using regex */
+                    let now = Instant::now();
+                    let result2 = document::search_using_regex(&documents.doc, &input);
+                    let elapsed = now.elapsed();
+                    for ids in result2 {
+                        let i32_ids = ids as i32;
+                        println!("Result using regex: {:?}", documents.doc.get(i32_ids as usize).unwrap().title);
+                    }
+                    println!("Elapsed: {:.6?}", elapsed);  
                     let tokenized = document::tokenize(&input);
                     if tokenized.len() == 0 {
                         println!("No results found");
-                    } else {
-                        let now = Instant::now();            
-                        
+                    } else {                        
+                        /* Search using full-text-search */
+                        let now = Instant::now();                                    
                         for token in tokenized {
                             if inverted_index.contains_key(&token) {
+                                
                                 for id in inverted_index.get(&token).unwrap() {
                                     println!("Result: {:?}", index_docs.get(id).unwrap().title);
                                 }
@@ -68,11 +73,5 @@ fn main() {
                         println!("Elapsed: {:.6?}", elapsed);
                     }                        
                 }
-            }
-            
-        },
-        Err(err) => {
-            println!("Error: {:?}", err);
-        }
-    } 
+    }
 }
